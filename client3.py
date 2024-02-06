bd93b5326fa9775f6da386a9023e3e3d294d9544
Neal L <rlawton.cs@gmail.com>
2024-02-05 22:51:40 -0800

    getDataPoint() now calculates the price correctly, getRatio() now returns the correct ratio, and the call to getRatio() in the main menthod now receives the correct arguments.

--- client3.py
+++ client3.py
@@ -35,25 +35,36 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    """ Also create some unit tests for this function in client_test.py """
+    if price_b == 0:
+        # Avoid a DivisionZeroError
+        return
+    return price_a / price_b
 
 
 # Main
+# For every iteration in the main method, you need to store the datapoints you get from
+# the getDataPoint method so that you can properly call getRatio and print the right ratio.
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        # Create a prices dictionary to store the stock prices.
+        # The key will be the stock name and the value will be the price.
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+        # Use the dictionary of prices to pass values to the getRatio() function.
+        # Print the ratio.
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
